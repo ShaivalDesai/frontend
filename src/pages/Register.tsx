b8394c49c3c1750db380f6a65b879faf219be56c
Shaival Desai <dshaival59@gmail.com>
2024-01-31 10:52:04 +0530

    project

---
+++ src/pages/Register.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+import AuthForm from "./AuthForm";
+
+const Register = () => {
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleRegister = () => {
+    // Registration logic here
+  };
+
+  return (
+    <AuthForm
+      formType="register"
+      credentials={credentials}
+      onChange={handleChange}
+      onSubmit={handleRegister}
+    />
+  );
+};
+
+export default Register;
