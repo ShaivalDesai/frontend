d51385e40f39a20cf41a8fc2345c41f87e3d0691
Shaival Desai <dshaival59@gmail.com>
2024-02-02 10:41:34 +0530

    updated_with_register_validation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,112 @@
-import React, { useState } from "react";
-import AuthForm from "./AuthForm";
+// import React, { useState } from "react";
+// import AuthForm from "./AuthForm";
+
+// const Login = () => {
+//   const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setCredentials({ ...credentials, [e.target.name]: e.target.value });
+//   };
+
+//   const handleLogin = () => {
+//     // Login logic here
+//   };
+
+//   return (
+//     <AuthForm
+//       formType="login"
+//       credentials={credentials}
+//       onChange={handleChange}
+//       onSubmit={handleLogin}
+//     />
+//   );
+// };
+
+// export default Login;
+
+import { LockOutlined } from "@mui/icons-material";
+import {
+  Container,
+  CssBaseline,
+  Box,
+  Avatar,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+} from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCredentials({ ...credentials, [e.target.name]: e.target.value });
-  };
-
-  const handleLogin = () => {
-    // Login logic here
-  };
+  const handleLogin = () => {};
 
   return (
-    <AuthForm
-      formType="login"
-      credentials={credentials}
-      onChange={handleChange}
-      onSubmit={handleLogin}
-    />
+    <>
+      <Container maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            mt: 20,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography variant="h5">Login</Typography>
+          <Box sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+            <Grid container justifyContent={"flex-end"}>
+              <Grid item>
+                <Link to="/register" style={{ marginRight: "35px" }}>
+                  Don't have an account? Register
+                </Link>
+                <Link to="/forgot-password">Forgot Password</Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </>
   );
 };
 
