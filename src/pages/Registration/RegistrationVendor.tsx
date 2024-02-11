36c51bf563fb4a5a19bb240e01e7005940cd54ff
Shaival Desai <dshaival59@gmail.com>
2024-02-11 20:45:19 +0530

    updated

--- src/pages/Registration/RegistrationVendor.tsx
+++ src/pages/Registration/RegistrationVendor.tsx
@@ -59,11 +59,7 @@ const RegistrationVendor = () => {
   // setImage(e.target.files[0])
   //  }
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setSelectedFile(event.target.files[0]);
-    }
-  };
+  
 
   const handleRegister = async () => {
     const validationErrors: Partial<Record<keyof FormData, string>> = {};
@@ -76,10 +72,6 @@ const RegistrationVendor = () => {
       validationErrors.vemail = "Email is required";
     } else if (!/^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(vemail)) {
       validationErrors.vemail = "Email is invalid";
-    }
-
-    if (!role.trim()) {
-      validationErrors.role = "Select Role";
     }
 
     if (!vgender.trim()) {
@@ -127,210 +119,275 @@ const RegistrationVendor = () => {
     if (Object.keys(validationErrors).length === 0) {
       try {
         const response = await axios.post(
-          "http://localhost:5000/api/register",
+          "http://127.0.0.1:8000/registration/vendor",
           {
             vname,
             vemail,
             vpwd,
-            role,
+            vnumber,
             vgender,
             vdob,
           }
         );
         console.log("response " + JSON.stringify(response.data));
         navigate("/home");
-      } catch (error) {
-        console.error("Error during registration:", error);
+      } catch (error: any) {
+        if (error.response) {
+          // Here you check for specific status codes or messages depending on your API
+          if (error.response.status === 400) {
+            // window.alert("Email already exists")
+            setErrors({ ...errors, vemail: "Email already exists" });
+          } else {
+            // Handle other errors or general error message
+            console.error("Error during registration:", error.message);
+          }
+        } else {
+          console.error("Error during registration:", error);
+        }
       }
     }
   };
 
   return (
-    <form onSubmit={handleRegister}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Paper elevation={3}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              padding: "20px",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
-              <LockOutlined />
-            </Avatar>
-            <Typography variant="h5">Vendor Registration</Typography>
-            <Box sx={{ mt: 3 }}>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    name="vname"
-                    required
-                    fullWidth
-                    id="vname"
-                    label="Name"
-                    autoFocus
-                    value={vname}
-                    onChange={(e) => setVname(e.target.value)}
-                  />
-                  <div style={{ color: "red" }}>
-                    {errors.vname && <span>{errors.vname}</span>}
-                  </div>
+    <div
+      style={{
+        height: "115vh",
+        display: "flex",
+        justifyContent: "center",
+
+        alignItems: "center",
+        backgroundImage: `url("/p1.jpeg")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <form onSubmit={handleRegister}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Paper elevation={3} sx={{ borderRadius: "15px" }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                padding: "20px",
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: "#724C31" }}>
+                <LockOutlined />
+              </Avatar>
+              <Typography variant="h5" sx={{ color: "#724C31" }}>
+                Vendor Registration
+              </Typography>
+              <Box sx={{ mt: 3 }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      name="vname"
+                      required
+                      fullWidth
+                      id="vname"
+                      label="Name"
+                      autoFocus
+                      value={vname}
+                      onChange={(e) => setVname(e.target.value)}
+                      InputLabelProps={{
+                        style: { color: "#724C31" },
+                      }}
+                    />
+                    <div style={{ color: "red" }}>
+                      {errors.vname && <span>{errors.vname}</span>}
+                    </div>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      value={vemail}
+                      onChange={(e) => setVemail(e.target.value)}
+                      InputLabelProps={{
+                        style: { color: "#724C31" },
+                      }}
+                    />
+                    <div style={{ color: "red" }}>
+                      {errors.vemail && <span>{errors.vemail}</span>}
+                    </div>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="vnumber"
+                      label="Phone Number"
+                      name="vnumber"
+                      value={vnumber}
+                      onChange={(e) => setVnumber(e.target.value)}
+                      inputProps={{ maxLength: 10 }}
+                      InputLabelProps={{
+                        style: { color: "#724C31" },
+                      }}
+                    />
+                    <div style={{ color: "red" }}>
+                      {errors.vnumber && <span>{errors.vnumber}</span>}
+                    </div>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <FormControl fullWidth required variant="outlined">
+                      <InputLabel
+                        id="gender-label"
+                        style={{ color: "#724C31" }}
+                      >
+                        Gender
+                      </InputLabel>
+                      <Select
+                        labelId="gender-label"
+                        id="vgender"
+                        name="vgender"
+                        value={vgender}
+                        onChange={(e) => setVgender(e.target.value)}
+                        label="Gender"
+                      >
+                        <MenuItem value="Male" style={{ color: "#724C31" }}>
+                          Male
+                        </MenuItem>
+                        <MenuItem value="Female" style={{ color: "#724C31" }}>
+                          Female
+                        </MenuItem>
+                        <MenuItem value="Other" style={{ color: "#724C31" }}>
+                          Other
+                        </MenuItem>
+                      </Select>
+                    </FormControl>
+
+                    <div style={{ color: "red" }}>
+                      {errors.vgender && <span>{errors.vgender}</span>}
+                    </div>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      type="date"
+                      id="dob"
+                      label="Date of Birth"
+                      name="dob"
+                      // InputLabelProps={{ shrink: true }}
+                      value={vdob}
+                      onChange={(e) => setVdob(e.target.value)}
+                      InputLabelProps={{
+                        shrink: true,
+                        style: { color: "#724C31" },
+                      }}
+                    />
+
+                    <div style={{ color: "red" }}>
+                      {errors.vdob && <span>{errors.vdob}</span>}
+                    </div>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type={showPassword ? "text" : "password"}
+                      id="password"
+                      value={vpwd}
+                      onChange={(e) => setVpwd(e.target.value)}
+                      InputLabelProps={{
+                        style: { color: "#724C31" },
+                      }}
+                      InputProps={{
+                        endAdornment: (
+                          <IconButton
+                            onClick={() => setShowPassword(!showPassword)}
+                            edge="end"
+                          >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        ),
+                      }}
+                    />
+
+                    <div style={{ color: "red" }}>
+                      {errors.vpwd && <span>{errors.vpwd}</span>}
+                    </div>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      type={showConfirmPassword ? "text" : "password"}
+                      id="confirmPassword"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      InputLabelProps={{
+                        style: { color: "#724C31" },
+                      }}
+                      InputProps={{
+                        endAdornment: (
+                          <IconButton
+                            onClick={() =>
+                              setShowConfirmPassword(!showConfirmPassword)
+                            }
+                            edge="end"
+                          >
+                            {showConfirmPassword ? (
+                              <VisibilityOff />
+                            ) : (
+                              <Visibility />
+                            )}
+                          </IconButton>
+                        ),
+                      }}
+                    />
+
+                    <div style={{ color: "red" }}>
+                      {errors.confirmPassword && (
+                        <span>{errors.confirmPassword}</span>
+                      )}
+                    </div>
+                  </Grid>
                 </Grid>
-
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    value={vemail}
-                    onChange={(e) => setVemail(e.target.value)}
-                  />
-                  <div style={{ color: "red" }}>
-                    {errors.vemail && <span>{errors.vemail}</span>}
-                  </div>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  sx={{
+                    mt: 3,
+                    mb: 2,
+                    bgcolor: "#724C31",
+                    "&:hover": {
+                      bgcolor: "#4A2F21", // Change background color to dark brown on hover
+                    },
+                  }}
+                  onClick={handleRegister}
+                >
+                  Register
+                </Button>
+                <Grid container justifyContent="flex-end">
+                  <Grid item>
+                    <Link to="/login" style={{ color: "#724C31" }}>
+                      Already have an account? Login
+                    </Link>
+                  </Grid>
                 </Grid>
-
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="vnumber"
-                    label="Phone Number"
-                    name="vnumber"
-                    value={vnumber}
-                    onChange={(e) => setVnumber(e.target.value)}
-                    inputProps={{ maxLength: 10 }}
-                  />
-                  <div style={{ color: "red" }}>
-                    {errors.vnumber && <span>{errors.vnumber}</span>}
-                  </div>
-                </Grid>
-
-                <Grid item xs={12}>
-                  <FormControl fullWidth required variant="outlined">
-                    <InputLabel id="gender-label">Gender</InputLabel>
-                    <Select
-                      labelId="gender-label"
-                      id="vgender"
-                      name="vgender"
-                      value={vgender}
-                      onChange={(e) => setVgender(e.target.value)}
-                      label="Gender"
-                    >
-                      <MenuItem value="Male">Male</MenuItem>
-                      <MenuItem value="Female">Female</MenuItem>
-                      <MenuItem value="Other">Other</MenuItem>
-                    </Select>
-                  </FormControl>
-
-                  <div style={{ color: "red" }}>
-                    {errors.vgender && <span>{errors.vgender}</span>}
-                  </div>
-                </Grid>
-
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    type="date"
-                    id="dob"
-                    label="Date of Birth"
-                    name="dob"
-                    InputLabelProps={{ shrink: true }}
-                    value={vdob}
-                    onChange={(e) => setVdob(e.target.value)}
-                  />
-
-                  <div style={{ color: "red" }}>
-                    {errors.vdob && <span>{errors.vdob}</span>}
-                  </div>
-                </Grid>
-
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type={showPassword ? "text" : "password"}
-                    id="password"
-                    value={vpwd}
-                    onChange={(e) => setVpwd(e.target.value)}
-                    InputProps={{
-                      endAdornment: (
-                        <IconButton
-                          onClick={() => setShowPassword(!showPassword)}
-                          edge="end"
-                        >
-                          {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      ),
-                    }}
-                  />
-
-                  <div style={{ color: "red" }}>
-                    {errors.vpwd && <span>{errors.vpwd}</span>}
-                  </div>
-                </Grid>
-
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="confirmPassword"
-                    label="Confirm Password"
-                    type={showConfirmPassword ? "text" : "password"}
-                    id="confirmPassword"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    InputProps={{
-                      endAdornment: (
-                        <IconButton
-                          onClick={() =>
-                            setShowConfirmPassword(!showConfirmPassword)
-                          }
-                          edge="end"
-                        >
-                          {showConfirmPassword ? (
-                            <VisibilityOff />
-                          ) : (
-                            <Visibility />
-                          )}
-                        </IconButton>
-                      ),
-                    }}
-                  />
-
-                  <div style={{ color: "red" }}>
-                    {errors.confirmPassword && (
-                      <span>{errors.confirmPassword}</span>
-                    )}
-                  </div>
-                </Grid>
-              </Grid>
-              <Button
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                onClick={handleRegister}
-              >
-                Register
-              </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link to="/login">Already have an account? Login</Link>
-                </Grid>
-              </Grid>
+              </Box>
             </Box>
-          </Box>
-        </Paper>
-      </Container>
-    </form>
+          </Paper>
+        </Container>
+      </form>
+    </div>
   );
 };
 export default RegistrationVendor;
