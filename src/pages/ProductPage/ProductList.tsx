04c99bd54da8e84c8743f84fbc8accb163befcc2
Shaival Desai <dshaival59@gmail.com>
2024-03-12 09:56:11 +0530

    updated

--- src/pages/ProductPage/ProductList.tsx
+++ src/pages/ProductPage/ProductList.tsx
@@ -908,6 +908,8 @@
 // };
 
 // export default ProductPag
+
+
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
